b21fc300dfa3d8caac378012d954203867c5a9f2
Aniket Bhunia <bhuniaaniket70@gmail.com>
2023-08-07 20:30:45 +0530

    A Components Folder Created and product_list component added inside it.

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListsComponent } from './Components/product-lists/product-lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListsComponent
   ],
   imports: [
     BrowserModule,
