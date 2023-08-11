3fc0586958577ce11804490d24caed34380c715b
TUBRI181 <tubri181@gmail.com>
2023-08-11 17:15:59 +0530

    update homepage UI

--- product-webapp/webapp/src/app/Components/home/home.component.ts
+++ product-webapp/webapp/src/app/Components/home/home.component.ts
@@ -7,13 +7,55 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 
-export class HomeComponent  {
+export class HomeComponent{
 
   // eslint-disable-next-line @typescript-eslint/ban-types
   search : String ="";
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
   
+  data1 = [
+    {
+      image: '../../assets/headphones.png',
+      name: 'Boat Headphones',
+      price: '₹ 1,299'
+    },
+    {
+      image: '../../assets/speaker.jpg',
+      name: 'JBL Speaker',
+      price: '₹ 2,249'
+    },
+    {
+      image: '../../assets/iPhone.jpg',
+      name: 'Iphone 14, 256GB',
+      price: '₹ 83,999'
+    },
+    {
+      image: '../../assets/toaster.jpg',
+      name: 'Prestige Toaster',
+      price: '₹ 799'
+    }
+  ]
+
+  data2 = [
+    {
+      image: '../../assets/sunglasses.jpg',
+      name: 'Rayban Sunglasses',
+      price: '₹ 2,999'
+    },
+    {
+      image: '../../assets/wallet.jpeg',
+      name: 'Raymond Mens Wallet',
+      price: '₹ 1,999'
+    },
+    {
+      image: '../../assets/laptop.png',
+      name: 'HP Probook X100',
+      price: '₹ 49,999'
+    }
+  ]
+
+  // ngOnInit(): void {
+  //   // Add any initialization logic here if needed.
+  // }
 
 }
 
