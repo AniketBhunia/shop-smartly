a8b9c62d04b4c1c67bbf4eb115d06c6a30fe34c7
suchismitabose2000 <suchismitabose2000@gmail.com>
2023-08-11 12:05:29 +0530

    integration updated

--- order-service/src/main/java/com/stackroute/orderservice/controller/CartController.java
+++ order-service/src/main/java/com/stackroute/orderservice/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CartController {
 
     private final CartService cartService;
