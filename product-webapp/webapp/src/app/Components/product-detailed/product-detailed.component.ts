c8e8f7c617d919b027c543be9e932740239e6d5d
sejalbarnwal1708 <sejalbarnwal1708@gmail.com>
2023-08-14 09:58:18 +0530

    Review Service integrated

--- product-webapp/webapp/src/app/Components/product-detailed/product-detailed.component.ts
+++ product-webapp/webapp/src/app/Components/product-detailed/product-detailed.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/Services/product.service';
 import { ReviewService } from 'src/app/Services/review.service';
 import { Product, Review } from 'src/app/data.types';
 import { Router, Params } from '@angular/router';
 import { FormGroup } from '@angular/forms';
+import { AlertService } from 'src/app/Services/alert-service';
 @Component({
   selector: 'app-product-detailed',
   templateUrl: './product-detailed.component.html',
   styleUrls: ['./product-detailed.component.css'],
 })
-export class ProductDetailedComponent implements OnInit {
+export class ProductDetailedComponent implements OnInit, AfterViewInit {
   productData!: Product;
   similarProducts!: Product[];
   categoryValue: string | null = null;
@@ -19,15 +26,17 @@ export class ProductDetailedComponent implements OnInit {
   reviewForm!: FormGroup;
   items: any[] = [];
   allPages: any[] = [];
-
+  fileName!: File;
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  @ViewChild('previewImage') previewImage!: ElementRef;
   constructor(
     private productService: ProductService,
     private activate: ActivatedRoute,
-    private reviewService: ReviewService
+    private reviewService: ReviewService,
+    private alertService: AlertService
   ) {}
 
   ngOnInit() {
-    this.getAllReviewsAndLimitPages();
     let productId = this.activate.snapshot.paramMap.get('productId');
     this.getReviewByProductId(productId);
 
@@ -58,14 +67,17 @@ export class ProductDetailedComponent implements OnInit {
   getAllReviewsAndLimitPages() {
     this.reviewService.reviewList().subscribe((res: any) => {
       this.reviewLists = res.content;
-      console.log(this.reviewLists);
+      console.log(this.reviewLists, 'this.reviews');
       let pageNumbers: any = [];
       for (let i = 1; i <= this.totalPages; i++) {
         pageNumbers.push(i);
       }
       this.allPages = pageNumbers;
-      
+      console.log(this.allPages, ' all PAges');
     });
+  }
+  ngAfterViewInit(): void {
+    this.getAllReviewsAndLimitPages();
   }
   // get review for productId
 
@@ -75,7 +87,6 @@ export class ProductDetailedComponent implements OnInit {
     console.log('value', value);
     this.value = value;
   }
-  
 
   reviewsPerPage: number = 4; // Number of reviews per page
   currentPage: number = 1;
@@ -92,7 +103,6 @@ export class ProductDetailedComponent implements OnInit {
     return this.reviewData.slice(startIndex, endIndex);
   }
 
-  
   limitedItems: any[] = [];
   // Change the current page and update the displayed reviews
   changePage(page: number): void {
@@ -111,19 +121,59 @@ export class ProductDetailedComponent implements OnInit {
       console.log(this.limitedItems, ' limiteditems');
     });
   }
-  createReview() {
-    const data = {
-      productReview: {
-      
-      },
-      image: '',
+  onFileSelected(event: any) {
+    this.fileName = event.target.files[0];
+    console.log(this.fileName);
+    if (this.fileName.size > this.MAX_FILE_SIZE) {
+      this.alertService.message('you can not upload file above 2mb');
+      return;
+    }
+    if (this.fileName.type === 'image/jpeg' || ' image/png') {
+      this.previewImage.nativeElement.src = window.URL.createObjectURL(
+        this.fileName
+      );
+      this.alertService.success('SuccessFully Uploaded');
+    } else {
+      this.alertService.error('you can not upload this file');
+    }
+  }
+
+  addReview(data: any) {
+    const demo = {
+      productId: 25,
+      user_name: data.name,
+      product_review_rating: data.rating,
+      product_review_description: data.review,
+      user_id: 44,
+      review_id: Math.floor(Math.random() * 500),
     };
+
     this.reviewService
-      .postReview(data.productReview, data.image)
+      .postReview(JSON.stringify(demo), this.fileName)
       .subscribe((res) => {
-        console.log(res);
+        this.alertService.success('successFully Added');
       });
   }
 
-  
+  url: any;
+  MAX_FILE_SIZE: number = 1 * 1000 * 1000;
+  onImageSelect(val: any) {
+    this.fileName = val.srcElement.files[0];
+    if (this.fileName.size > this.MAX_FILE_SIZE) {
+      this.alertService.message('you can not upload file above 2mb');
+      return;
+    }
+    if (this.fileName.type === 'image/jpeg' || ' image/png') {
+      this.previewImage.nativeElement.src = window.URL.createObjectURL(
+        this.fileName
+      );
+      this.alertService.success('SuccessFully Uploaded');
+    } else {
+      this.alertService.error('you can not upload this file');
+    }
+  }
+
+  selectImage() {
+    this.fileInput.nativeElement.click();
+  }
 }
